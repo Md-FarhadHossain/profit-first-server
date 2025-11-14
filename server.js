b2f878155c4102af3d5891a1d2fa40721e5970b7
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2025-11-15 05:03:27 +0600

    order get and post

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express")
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config()
+const cors = require("cors")
+const port = 5000
+
+app.use(express.json())
+app.use(cors())
+
+
+const uri = `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.1ivadd4.mongodb.net/?appName=Cluster0`
+console.log(uri)
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const dbConnect = async () => {
+  try {
+    await client.connect();
+    console.log("Database connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+dbConnect();
+
+const allOrders = client.db("profit-first").collection("allOrders");
+
+
+app.get('/', (req, res) => {
+    res.send("Hi")
+})
+
+
+app.post("/orders", async (req, res) => {
+  try {
+    const order = req.body;
+    const result = await allOrders.insertOne(order);
+    res.send(result);
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
+});
+
+app.get("/orders", async (req, res) => {
+  try {
+    const query = {};
+    const result = await allOrders.find(query).toArray();
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {  
+    console.log(`server is running ${port}`)
+})
